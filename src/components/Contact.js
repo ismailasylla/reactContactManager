53a27f34df904e6314c16359f8a1ec9e287f4165
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2018-08-29 21:03:32 +0530

    3. Changing State

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,21 +2,36 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
+
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">
-            Email:
-            {email}
-          </li>
-          <li className="list-group-item">
-            Phone:
-            {phone}
-          </li>
-        </ul>
+        <h4>
+          {name}{' '}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+          />{' '}
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">
+              Email:
+              {email}
+            </li>
+            <li className="list-group-item">
+              Phone:
+              {phone}
+            </li>
+          </ul>
+        ) : null}
       </div>
     );
   }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -30,11 +30,11 @@ class Contacts extends Component {
     const { contacts } = this.state;
 
     return (
-      <div>
+      <React.Fragment>
         {contacts.map(contact => (
           <Contact key={contact.id} contact={contact} />
         ))}
-      </div>
+      </React.Fragment>
     );
   }
 }
