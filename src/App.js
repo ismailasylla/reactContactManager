0a6317513f0a546a844166c6f35a27d19044f847
Syldox <syldox@gmail.com>
2018-09-02 19:15:34 +0530

    setting up 404 Not Found Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Provider } from './context';
 import AddContact from './components/contacts/AddContact';
 import Header from './components/layout/Header';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
                 <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">
+        <b className="text-danger">404</b> Page Not Found
+      </h1>
+      <p className="lead">
+        <b>Sorry</b> That Page does not exit !
+      </p>
+    </div>
+  );
+};
